2dfc319d9e578e7a201f9a52265e70937a95171c
Paul Chung <paulchung39@gmail.com>
2017-08-02 20:23:52 -0700

    updated travis yml

--- semaphore/semaphore_app/tests/tests.py
+++ semaphore/semaphore_app/tests/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from ..model import *
+
+class InstanceTest(TestCase):
+
+	def setUp(self):
+		Instance.objects.create(
+				
+       
+
 
 # Create your tests here.
